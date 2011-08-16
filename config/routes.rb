10c89bd6fc0ce0cebcfe902ebe2f37c07b486dc3
Tekin <tekin@tekin.co.uk>
2011-08-16 22:17:22 +0100

    Add a "match" route that routes to a static page

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,5 @@ CoursesApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  match 'about' => 'pages#about'
 end
