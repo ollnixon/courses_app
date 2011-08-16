0413c174c2d672e413452f23496010c22d37b035
Tekin <tekin@tekin.co.uk>
2011-08-16 20:24:14 +0100

    Format Nazi: newline at end of file...

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 Course.create(:name => 'Ruby & Rails Bootcamp', 
               :description => 'Learn how to develop high-quality web applications fast using the renowned web framework that powers Twitter, Groupon, Github, Basecamp and more.',
               :start_date => "2011-08-17")
