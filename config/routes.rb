cceac40d9019ca17050fe7b4f68be316457fe887
Tekin <tekin@tekin.co.uk>
2011-08-16 20:36:17 +0100

    Added skeleton layout and controller for Courses with a bare bones index template.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 CoursesApp::Application.routes.draw do
+  resources :courses
+
+  root :to => 'courses#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
