45946caeacdae3e577de8acaca4ac3e8dbcfc1f8
Oliver Nixon <oliver@clockhill.co.uk>
2011-08-18 14:40:04 +0100

    Validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+  validates :name, :presence => true, :length => { :minimum => 6 }
+  validates :description, :presence => true
+  validates :price, :numericality => true, :allow_nil => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, :presence => true, :length => { :minimum => 3 }
+  validates :email, :presence => true, :uniqueness => true
+  validates_as_email_address :email
+  #validates_presence_of :name
 end
