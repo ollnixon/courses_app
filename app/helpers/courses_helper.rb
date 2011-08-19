7e37bb853abb887cc1115fa2b2b074c6759a231a
Oliver Nixon <oliver@clockhill.co.uk>
2011-08-19 13:19:17 +0100

    Links

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,13 @@
 module CoursesHelper
+  def time_until_start(course)
+    days = (course.start_date - Date.today).to_i
+    
+    if days < 1
+      "course has already started"
+    else
+      "#{distance_of_time_in_words course.start_date, Date.today}"
+      "#{distance_of_time_in_words_to_now course.start_date}"
+      #"#{pluralize days, 'day'} to go"
+    end
+  end
 end
