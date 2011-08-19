fc7eda86c3085501cfd42f15dbccaec9c208f49b
Oliver Nixon <oliver@clockhill.co.uk>
2011-08-19 14:39:28 +0100

    new and updates

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,4 +6,36 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id])
   end
+
+  def new
+    @course = Course.new
+    #@course.start_date = Date.today + 1
+  end
+
+  def create
+    @course = Course.new(params[:course])
+    if @course.save
+      redirect_to @course, :notice => "New course created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update_attributes(params[:course])
+      redirect_to @course, :notice => "Course updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Course.destroy(params[:id])
+    redirect_to :index, :notice => "Course deleted"
+  end
 end
